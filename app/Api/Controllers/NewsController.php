eebf355b3d66a561c6bf23638a4cb42879f67dd9
mvp-chain <mvp.xuan@gmail.com>
2018-05-30 16:43:12 +0800

    Change config: image full path.

--- app/Api/Controllers/NewsController.php
+++ app/Api/Controllers/NewsController.php
@@ -15,6 +15,9 @@ class NewsController extends Controller
     public function index()
     {
         $data = News::where('status', '1')->orderBy('id', 'desc')->paginate(10);
+        foreach ($data as &$item) {
+            $item['focus_img_url'] = env('MY_API_HTTP_HEAD', 'http://localhost/') . $item['focus_img_url'];
+        }
 
         $retData = [
             'code' => 1000,
